3329260f0daefce254b9d24a7bc9b0dafe60dd32
JordyBacherot <jordy.bacherot@outlook.fr>
2025-03-21 11:35:53 +0100

    Adding Hobbies with screw Adding Responsive for Hobbies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Cv from './CV/Cv'
 import Background from "./Background/Background.tsx";
 import Projects from "./Projects/Projects.tsx";
+import Hobbies from "./Hobbies/Hobbies.tsx";
 
 function App() {
     return (
+    //     Todo : redesign mobile responsive
     <>
         <Background>
             <header className="py-5 xl:max-w-7xl mx-auto lg:max-w-[900px]">
@@ -18,11 +20,22 @@ function App() {
                     lg:mr-10 mt-5 lg:col-span-7">
                         <Cv />
                     </div>
-                    <div className="col-start-3 col-end-19 mt-15
-                    xl:col-start-8 xl:col-end-20 xl:p-5
-                    lg:col-start-8 lg:col-end-20 lg:p-0 lg:ml-18
+                    <div className="col-start-3 col-end-19 mt-13
+                    md:mt-10
+                    xl:col-start-8 xl:col-end-20 xl:p-5 xl:mt-12
+                    lg:col-start-8 lg:col-end-20 lg:p-0 lg:ml-18 lg:mt-15
                     md:p-15 md:col-start-2 md:col-end-20">
                         <Projects />
+                    </div>
+                    <div className="col-start-1 col-end-21 mt-15
+                    md:col-start-4 md:col-end-18 md:mt-10
+                    lg:col-start-1 lg:col-end-10 lg:mt-15">
+                        <Hobbies />
+                    </div>
+                    <div className="col-start-1 col-end-21 mt-8
+                    md:col-start-4 md:col-end-18 md:mt-10
+                    lg:col-start-12 lg:col-end-21 lg:mt-15">
+                        <Hobbies />
                     </div>
                 </div>
             </div>
---
+++ src/Hobbies/Hobbies.tsx
@@ -0,0 +1,70 @@
+import './Hobbies.css';
+import { useState } from 'react';
+import screwImg from '../assets/img/screw.svg';
+
+function Hobbies() {
+    const [screwState, setScrewState] = useState([true, true, true, true]);
+
+    const positions = [
+        'top-1 left-1',    // Haut gauche
+        'top-1 right-1',   // Haut droite
+        'bottom-1 left-1', // Bas gauche
+        'bottom-1 right-1' // Bas droite
+    ];
+
+    function animateScrew(index: number) {
+        const screw = document.getElementById(`screw-${index}`);
+
+        // Ajouter la classe d'animation pour faire tourner et disparaître la vis
+        screw?.classList.add('animate-screw');
+
+        // Attendre la fin de l'animation avant de mettre à jour l'état
+        setTimeout(() => {
+            // Mise à jour de l'état pour retirer la vis
+            setScrewState((prevState) => {
+                const updatedState = [...prevState];
+                updatedState[index] = false;
+
+                // Si toutes les vis sont retirées, déclencher l'animation du bloc
+                if (updatedState.every((screw) => !screw)) {
+                    const falling_bloc = document.getElementById('falling_bloc');
+                    falling_bloc?.classList.add('animate-falling');
+                }
+
+                return updatedState;
+            });
+        }, 1500); // Correspond à la durée de l'animation de la vis
+    }
+
+    function drawScrew() {
+        const tab_screw = [];
+        for (let i = 0; i < 4; i++) {
+            if (screwState[i]) {
+                tab_screw.push(
+                    <img
+                        id={`screw-${i}`}
+                        key={i}
+                        src={screwImg}
+                        alt="screw"
+                        className={`absolute ${positions[i]} w-5 h-5 z-10 screw-img`}
+                        onClick={() => animateScrew(i)}
+                    />
+                );
+            }
+        }
+        return tab_screw;
+    }
+
+    return (
+        <div className="relative w-full h-30 rounded-2xl bg-[#CE8147] overflow-hidden">
+            <div id="falling_bloc" className="absolute top-0 left-0 w-full rounded-2xl h-full cv-color-bg">
+                <div className="relative w-full h-full flex items-center justify-center z-10">
+                    <h2 className="md:text-6xl font-bold color-text font-rakkas text-5xl">Hobbies</h2>
+                </div>
+                {drawScrew()}
+            </div>
+        </div>
+    );
+}
+
+export default Hobbies;
